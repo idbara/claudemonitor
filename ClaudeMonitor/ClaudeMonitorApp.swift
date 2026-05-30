c8626127cf5bddfd79eadd890f01abcac19cbcee
Bara Ramadhan <bararamadhan@gmail.com>
2026-05-30 21:49:47 +0700

    Initial Commit

---
+++ ClaudeMonitor/ClaudeMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeMonitorApp.swift
+//  ClaudeMonitor
+//
+//  Created by Bara Ramadhan on 30/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
